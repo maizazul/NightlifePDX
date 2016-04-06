fe254665e1e9873eabbd5119959608b5e1d54a9b
Eben Wood <ebenewood@yahoo.com>
2016-04-06 16:28:47 -0700

    creates map centered on user OR pdx, creates search with autofill from array

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+var clickArray = ['pizza'];
+
+
+var myCallback = function() {
+
+  if (document.readyState == 'complete') {
+    // Document is ready when CSE element is initialized.
+    // Render an element with both search box and search results in div with id 'test'.
+    google.search.cse.element.render(
+        { gname: 'a',
+          div: "test",
+          tag: 'search'
+         });
+  } else {
+    // Document is not ready yet, when CSE element is initialized.
+    google.setOnLoadCallback(function() {
+       // Render an element with both search box and search results in div with id 'test'.
+        google.search.cse.element.render(
+            {
+              gname: 'a',
+              div: "test",
+              tag: 'search'
+            });
+            var element = google.search.cse.element.getElement('a');
+                element.execute(clickArray[0]);
+    }, true);
+  }
+};
+
+// Insert it before the CSE code snippet so that cse.js can take the script
+// parameters, like parsetags, callbacks.
+window.__gcse = {
+  parsetags: 'explicit',
+  callback: myCallback
+};
+
+(function() {
+  var cx = '004427760427619622683:nxngoboo2bi'; // Insert your own Custom Search engine ID here
+  var gcse = document.createElement('script'); gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
